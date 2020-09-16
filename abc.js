916886e9328c5907f6e1f7590977b0cd2f4d8847
thanhtung-1994 <71205530+tung-94@users.noreply.github.com>
2020-09-16 19:40:12 +0700

    creat new project

---
+++ abc.js
@@ -0,0 +1,85 @@
+Bài 1:
+{
+let a;
+let b;
+a= prompt("What is your name");
+b= prompt("How old are you ");
+alert(`${a} is ${b} years old`);
+}
+Bài 2:
+{
+    let name, name2, name3, name4, name5;
+let old, old2, old3, old4, old5;
+let i=1;
+name= prompt(`P${i}: What is your name`);
+old= prompt("How old are you ");
+i++;
+name2= prompt(`P${i}: What is your name`);
+old2= prompt("How old are you ");
+i++;
+name3= prompt(`P${i}: What is your name`);
+old3= prompt("How old are you ");
+i++;
+name4= prompt(`P${i}: What is your name`);
+old4= prompt("How old are you ");
+i++;
+name5= prompt(`P${i}: What is your name`);
+old5= prompt("How old are you ");
+i++;
+console.log(`${name} is ${old} years old`);
+console.log(`${name2} is ${old2} years old`);
+console.log(`${name3} is ${old3} years old`);
+console.log(`${name4} is ${old4} years old`);
+console.log(`${name5} is ${old5} years old`);
+}
+Bài 3:
+{
+    var messenger ="Coding is great";
+console.log(messenger);
+var StudentCount =0;
+console.log(StudentCount);
+}
+Bài 4:
+{
+    var messenger = "Coding might not be easy, but still great";
+console.log(messenger);
+messenger= messenger.toUpperCase();
+console.log(messenger);
+var studentCount= 16;
+console.log(studentCount);
+studentCount++;
+console.log(studentCount);
+}
+Bài 5:
+{
+    alert("Good luck!");
+}
+Bài 6:
+{
+    var name= prompt("Hi there, your name please!");
+alert(`Hi ${name}`);
+}
+Bài 7:
+{
+    var firstName = prompt("Enter your first name:");
+var lastName= prompt("Enter your last name");
+alert(`Hi ${firstName} ${lastName}`);
+}
+Bài 8:
+{
+    var l= prompt("Enter side lenght of square");
+var area= l*l;
+alert(`Area of square is ${area}`);
+}
+Bài 9:
+{
+    var r= prompt("Enter radius of circle");
+var area= r*r*3.14;
+alert(`The circle area is ${area}`);
+}
+Bài 10:
+{
+    var c= prompt("Enter temperature in Celsius");
+var f= c*1.8+32;
+alert(`${c}(C) = ${f} (F)`);
+}
